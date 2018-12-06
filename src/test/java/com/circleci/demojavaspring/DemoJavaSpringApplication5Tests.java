ed8b60d415f1de27d4a0fd7dd1d62dc227665f1f
Noboru Kurumai <noboru@circleci.com>
2018-12-06 09:40:42 +0900

    use environment variables

---
+++ src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplication5Tests.java
@@ -0,0 +1,21 @@
+package com.circleci.demojavaspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration
+@TestPropertySource("/test.properties")
+public class DemoJavaSpringApplication5Tests {
+
+	@Test
+	public void contextLoads() throws Throwable{
+		Thread.sleep(10 * 1000);
+	}
+
+}
